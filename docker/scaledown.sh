5b2a0d2c9cac59b8d903e148a291ec8b4ad0423c
Greg Hellings <greg@thesub.net>
2017-03-14 12:53:00 -0700

    Fixing case issue with scaledown

--- docker/scaledown.sh
+++ docker/scaledown.sh
@@ -3,9 +3,9 @@ set -e
 echo $* 1>&2
 
 ES_SERVICE_NAME=`echo $DOCKERCLOUD_SERVICE_HOSTNAME | sed "s/-scaledown//"`
-LAST_ES_IP=`set | egrep -e "${ES_SERVICE_NAME}_[0-9]+_PORT_9200_TCP_ADDR" | cut -d'=' -f2 | tail -1`
-LAST_ES_NAME=`set | egrep -e "${ES_SERVICE_NAME}_[0-9]+_PORT_9200_TCP_ADDR" | sed "s/_PORT.*//" | tail -1 | awk '{print tolower($0)}'`
-NUM_ES_CONTAINERS=`set | egrep -e "${ES_SERVICE_NAME}_[0-9]+_PORT_9200_TCP_ADDR" | wc -l`
+LAST_ES_IP=`set | egrep -ie "${ES_SERVICE_NAME}_[0-9]+_PORT_9200_TCP_ADDR" | cut -d'=' -f2 | tail -1`
+LAST_ES_NAME=`set | egrep -ie "${ES_SERVICE_NAME}_[0-9]+_PORT_9200_TCP_ADDR" | sed "s/_PORT.*//" | tail -1 | awk '{print tolower($0)}'`
+NUM_ES_CONTAINERS=`set | egrep -ie "${ES_SERVICE_NAME}_[0-9]+_PORT_9200_TCP_ADDR" | wc -l`
 
 echo -e "es_service_name: $ES_SERVICE_NAME\nlast_es_ip: $LAST_ES_IP\nlast_es_name: $LAST_ES_NAME\nnum_es_containers: $NUM_ES_CONTAINERS\n"
 
@@ -21,7 +21,7 @@ while true; do curl -s http://${ES_SERVICE_NAME}:9200/_cat/shards | egrep -e "${
 ES_SERVICE_UUID=`\
 	curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}/api/app/v1/${ORG_NAME}service/?limit=250 |\
 	jq '.objects[] | .name + " " +.uuid' |\
-	egrep "$ES_SERVICE_NAME "|\
+	egrep -ie "$ES_SERVICE_NAME "|\
 	cut -d$' ' -f2 |
 	sed 's/"//g'`
 
