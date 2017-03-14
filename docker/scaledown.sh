30fa623bceccef84c51e40a9de1145d38d52f2cb
Greg Hellings <greg@thesub.net>
2017-03-14 13:11:44 -0700

    Fixing scaledown

--- docker/scaledown.sh
+++ docker/scaledown.sh
@@ -11,7 +11,8 @@ echo -e "es_service_name: $ES_SERVICE_NAME\nlast_es_ip: $LAST_ES_IP\nlast_es_nam
 
 
 # Evacuate shards
-curl -s -XPUT "http://${ES_SERVICE_NAME}:9200/_cluster/settings" -d "{ \"transient\" : { \"cluster.routing.allocation.exclude._ip\" : \"$LAST_ES_IP\" } }"
+curl -s -XPUT "http://${ES_SERVICE_NAME}:9200/_cluster/settings" \
+	-d "{ \"transient\" : { \"cluster.routing.allocation.exclude._ip\" : \"$LAST_ES_IP\" } }"
 
 # Wait for shards to finished evacuating
 echo "Evacuating shards"
@@ -19,7 +20,8 @@ while true; do curl -s http://${ES_SERVICE_NAME}:9200/_cat/shards | egrep -e "${
 
 # Get service UUID
 ES_SERVICE_UUID=`\
-	curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}/api/app/v1/${ORG_NAME}service/?limit=250 |\
+	curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" \
+		${DOCKERCLOUD_REST_HOST}/api/app/v1/${ORG_NAME}service/?limit=250 |\
 	jq '.objects[] | .name + " " +.uuid' |\
 	egrep -ie "$ES_SERVICE_NAME "|\
 	cut -d$' ' -f2 |
@@ -29,8 +31,14 @@ ES_SERVICE_UUID=`\
 # Scale Service
 echo "Scaling down cluster"
 TARGET_NUM_CONTAINERS=`echo "$NUM_ES_CONTAINERS - 1" | bc`
-curl -s -H "Content-Type: application/json" -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}/api/app/v1/${ORG_NAME}service/${ES_SERVICE_UUID}/ -XPATCH -d '{ "target_num_containers" : $TARGET_NUM_CONTAINERS }'
-curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}/api/app/v1/${ORG_NAME}service/${ES_SERVICE_UUID}/scale/ -XPOST
+curl -s -H "Content-Type: application/json" -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" \
+	${DOCKERCLOUD_REST_HOST}/api/app/v1/${ORG_NAME}service/${ES_SERVICE_UUID}/ \
+	-XPATCH \
+	-d "{ \"target_num_containers\" : $TARGET_NUM_CONTAINERS }"
+curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" \
+	${DOCKERCLOUD_REST_HOST}/api/app/v1/${ORG_NAME}service/${ES_SERVICE_UUID}/scale/ \
+	-XPOST
 
 # Remove exclude
-curl -s -XPUT "http://${ES_SERVICE_NAME}:9200/_cluster/settings" -d "{ \"transient\" : { \"cluster.routing.allocation.exclude._ip\" : \"\" } }"
+curl -s -XPUT "http://${ES_SERVICE_NAME}:9200/_cluster/settings" 
+	-d "{ \"transient\" : { \"cluster.routing.allocation.exclude._ip\" : \"\" } }"
